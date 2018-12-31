145f9002633f2ecd5df6c90ec9f7bf41b3093633
Daniel Etter <etter.dt@gmail.com>
2018-12-31 03:14:21 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def self.current_user(session)
+    User.find_by(id: session[:id])
+  end
+
+  def self.is_logged_in?(session)
+    if User.find_by(id: session[:id])
+      true
+    else
+      false
+    end
+  end
 end
